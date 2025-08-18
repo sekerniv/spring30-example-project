853e861bed32eb758971d8873bd45662d4c2b990
sekerniv <sekerniv@gmail.com>
2025-08-18 20:44:25 +0300

    updated characters to use bootstrap responsive grid and added an example for responsive grid

--- src/main/java/proj/ExampleController.java
+++ src/main/java/proj/ExampleController.java
@@ -5,13 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class ExampleController {
 
     @GetMapping("/example_bootstrap_grid")
-    public String about(Model model, HttpSession session) {
+    public String bootstrapGridExample(Model model, HttpSession session) {
         return "/examples/example_bootstrap_grid";
-    }     
+    }
 
+    @GetMapping("/example_bootstrap_responsive_grid")
+    public String bootstrapResponsiveGridExample(Model model, HttpSession session) {
+        return "/examples/example_bootstrap_responsive_grid";
+    }
 }
